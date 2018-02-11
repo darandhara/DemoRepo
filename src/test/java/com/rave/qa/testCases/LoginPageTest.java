244b5fddf7755fa5226fa5fdf978c586350f060f
darandhara <durlovchndn@gmail.com>
2018-02-11 01:08:45 -0800

    added log4j code

--- src/test/java/com/rave/qa/testCases/LoginPageTest.java
+++ src/test/java/com/rave/qa/testCases/LoginPageTest.java
@@ -6,12 +6,14 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
 import com.rave.qa.base.TestBase;
 import com.rave.qa.pages.LoginPage;
 
 public class LoginPageTest extends TestBase{
 	LoginPage loginPage;
+	Logger log=Logger.getLogger(LoginPageTest.class);
 	
 	public LoginPageTest() {
 		super();
@@ -20,6 +22,10 @@ public class LoginPageTest extends TestBase{
 	@BeforeMethod
 	public void setUp() {
 		initialization();
+		log.info("Here is the information log of log4j---------->>>>>>>>>Yes");
+		log.warn("Here is the warning log of log4j---------->>>>>>>>>Yes");
+		log.error("Here is the Error log of log4j---------->>>>>>>>>Yes");
+		log.fatal("Here is the fatal log of log4j---------->>>>>>>>>Yes");
 		loginPage =new LoginPage();
 	}
 	
@@ -27,6 +33,10 @@ public class LoginPageTest extends TestBase{
 	public void LoginPageTitleTest() {
 		String title=loginPage.validateLoginPageTitle();
 		Assert.assertEquals(title, "CRMPRO - CRM software for customer relationship management, sales, and support.");
+		log.info("Here is the information log of log4j---------->>>>>>>>>Yes");
+		log.warn("Here is the warning log of log4j---------->>>>>>>>>Yes");
+		log.error("Here is the Error log of log4j---------->>>>>>>>>Yes");
+		log.fatal("Here is the fatal log of log4j---------->>>>>>>>>Yes");
 	}
 	
 	
